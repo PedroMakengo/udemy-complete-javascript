45b16de3858a89fe432225332d0493b80f63b37f
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-09-01 07:07:05 -0300

    Terminando a parte condicional

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -9,13 +9,74 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 score = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
+document.querySelector(".dice").style.display = "none";
 
-document.querySelector();
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1. Random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. Display the result
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+
+  // 3. Update the round score IF the rolled number was NOT a 1
+  if (dice !== 1) {
+    // Add score
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    // Next player
+    nextPlayer();
+  }
+});
+
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  // Add current scoroe to Global score
+  score[activePlayer] += roundScore;
+  // score[activePlayer] = score[activePlayer] + roundScore;
+
+  // Update the UI
+  document.querySelector("#score-" + activePlayer).textContent =
+    score[activePlayer];
+
+  // Check if player won the game
+  if (score[activePlayer] >= 20) {
+    document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+    document.querySelector(".dice").style.display = "none";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.add("winner");
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.remove("active");
+  } else {
+    // Next player
+    nextPlayer();
+  }
+});
+
+function nextPlayer() {
+  // Next Player
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+
+  document.querySelector(".dice").style.display = "none";
+}
