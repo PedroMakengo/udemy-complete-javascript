c90faae0fb1eeea135ff72dc45d625e20c7d1018
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-09-03 15:44:23 -0300

    Trabalhando

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -46,37 +46,104 @@
 //
 // Primitives vs Objects
 
-var a = 23;
-var b = a;
+// var a = 23;
+// var b = a;
 
-a = 46;
-console.log(a);
-console.log(b);
+// a = 46;
+// console.log(a);
+// console.log(b);
 
-var obj1 = {
-  name: "John",
-  age: 26,
-};
+// var obj1 = {
+//   name: "John",
+//   age: 26,
+// };
 
-var obj2 = obj1;
-obj1.age = 30;
+// var obj2 = obj1;
+// obj1.age = 30;
 
-console.log(obj1.age);
-console.log(obj2.age);
+// console.log(obj1.age);
+// console.log(obj2.age);
 
-// Functions
-var age = 27;
-var obj = {
-  name: "Jonas",
-  city: "Lisbon",
-};
+// // Functions
+// var age = 27;
+// var obj = {
+//   name: "Jonas",
+//   city: "Lisbon",
+// };
 
-function change(a, b) {
-  a = 30;
-  b.city = "San Francisco";
+// function change(a, b) {
+//   a = 30;
+//   b.city = "San Francisco";
+// }
+
+// change(age, obj);
+
+// console.log(age);
+// console.log(obj.city);
+
+// Lecture Passing Function as arguments
+
+// var years = [1990, 1965, 1937, 2005, 1998];
+
+// function arrayCalc(arr, fn) {
+//   var arrRes = [];
+//   for (var i = 0; i < arr.length; i++) {
+//     arrRes.push(fn(arr[i]));
+//   }
+
+//   return arrRes;
+// }
+
+// function calculateAge(el) {
+//   return 2021 - el;
+// }
+
+// function isFullAge(el) {
+//   return el >= 18;
+// }
+
+// function maxHeartRate(el) {
+//   if (el >= 18 && el <= 81) {
+//     return Math.round(206.9 - 0.67 * el);
+//   } else {
+//     return -1;
+//   }
+// }
+
+// var ages = arrayCalc(years, calculateAge);
+
+// var fullAges = arrayCalc(ages, isFullAge);
+
+// var rates = arrayCalc(ages, maxHeartRate);
+
+// console.log(ages);
+// console.log(isFullAge);
+// console.log(rates);
+
+function interviewQuestion(job) {
+  if (job === "designer") {
+    return function (name) {
+      console.log(name + ", can you please explain waht UX design is ? ");
+    };
+  } else if (job === "teacher") {
+    return function (name) {
+      console.log("What subject do yo teacher, " + name + "?");
+    };
+  } else {
+    return function (name) {
+      console.log("Hello" + name + ", what do you do?");
+    };
+  }
 }
 
-change(age, obj);
+var teacherQuestion = interviewQuestion("teacher");
 
-console.log(age);
-console.log(obj.city);
+var designerQuestion = interviewQuestion("designer");
+
+teacherQuestion("John");
+designerQuestion("John");
+designerQuestion("Jane");
+designerQuestion("Mark");
+designerQuestion("Mike");
+
+interviewQuestion("teacher")("Mark");
