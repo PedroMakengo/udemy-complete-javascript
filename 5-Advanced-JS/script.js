58d3eff1838a67e0b760744f25e17035c3cb7934
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-09-04 12:44:43 -0300

    5-Advanced

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -260,18 +260,27 @@
     this.correct = correct;
   }
 
-  Question.prototype.displayQuestion = () => {
+  Question.prototype.displayQuestion = function () {
     console.log(this.question);
 
     for (var i = 0; i < this.answers.length; i++) {}
   };
 
-  Question.prototype.checkAnswer = function (ans) {
+  Question.prototype.checkAnswer = function (ans, callback) {
     if (ans === this.correct) {
       console.log("Correct answer!");
+      callback(true);
     } else {
       console.log("Wrong answer Try again :)");
+
+      sc = callback(false);
     }
+    this.displayScore(sc);
+  };
+
+  Question.prototype.displayScore = function (score) {
+    console.log("Your current score is: " + score);
+    console.log("-------------------------------");
   };
 
   var q1 = new Question(
@@ -292,9 +301,21 @@
     2
   );
 
-  function nextQuestion() {
-    var questions = [q1, q2, q3];
+  var questions = [q1, q2, q3];
+  function score() {
+    var sc = 0;
 
+    return function (correct) {
+      if (correct) {
+        sc++;
+      }
+      return sc;
+    };
+  }
+
+  var keepScore = score();
+
+  function nextQuestion() {
     var n = Math.floor(Math.random() * questions.length);
 
     questions[n];
@@ -302,7 +323,7 @@
     var answer = parseInt(prompt("Please select the correct answer"));
 
     if (answer !== "exit") {
-      questions[n].checkAnswer(parseInt(answer));
+      questions[n].checkAnswer(parseInt(answer), keepScore);
       nextQuestion();
     }
   }
